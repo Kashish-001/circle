7f553ba627f6a46809230c9f7e26975b0027767e
Anonymous_1 <53265299+shivamksharma@users.noreply.github.com>
2023-09-28 00:22:36 +0530

    New Commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let isDragging = false;
+let xOffset, yOffset;
+const card = document.getElementById('draggable');
 
+card.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    xOffset = e.clientX - card.offsetLeft;
+    yOffset = e.clientY - card.offsetTop;
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        card.style.left = `${e.clientX - xOffset}px`;
+        card.style.top = `${e.clientY - yOffset}px`;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
